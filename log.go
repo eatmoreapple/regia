7a7b1fa3e36130b121b518c6c24789e261360909
eatMoreApple <15055461510@163.com>
2021-06-24 13:13:28 +0800

    add log time format

--- log.go
+++ log.go
@@ -1,6 +1,9 @@
 package regia
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 const colorFormat = "\u001B[%dm%s\u001B[0m"
 
@@ -11,6 +14,15 @@ const (
 	colorBlue                // blue
 	colorMagenta             // magenta
 )
+
+const (
+	info   = "INFO"
+	debug  = "DEBUG"
+	warn   = "WARN"
+	danger = "DANGER"
+)
+
+const timeFormat = "2006-01-02 15:04:05"
 
 func formatColor(color int, text string) string {
 	return fmt.Sprintf(colorFormat, color, text)
@@ -26,19 +38,27 @@ type Log interface {
 type ConsoleLog struct{}
 
 func (c ConsoleLog) Info(text string) {
+	text = c.Format(info, text)
 	fmt.Println(formatColor(colorGreen, text))
 }
 
 func (c ConsoleLog) Debug(text string) {
+	text = c.Format(debug, text)
 	fmt.Println(formatColor(colorMagenta, text))
 }
 
 func (c ConsoleLog) Warn(text string) {
+	text = c.Format(warn, text)
 	fmt.Println(formatColor(colorYellow, text))
 }
 
 func (c ConsoleLog) Error(text string) {
+	text = c.Format(danger, text)
 	fmt.Println(formatColor(colorRed, text))
+}
+
+func (c ConsoleLog) Format(level, text string) string {
+	return fmt.Sprintf("[%-5s]  %-20s    %s", level, time.Now().Format(timeFormat), text)
 }
 
 var Logger Log = ConsoleLog{}
