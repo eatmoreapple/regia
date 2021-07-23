6f578f7f694a3bdbc010d543e7b659fe08cb5cc1
eatMoreApple <15055461510@163.com>
2021-07-23 17:17:59 +0800

    add json trans

--- render.go
+++ render.go
@@ -3,6 +3,7 @@ package regia
 import (
 	"encoding/json"
 	"encoding/xml"
+	"errors"
 	"net/http"
 )
 
@@ -14,7 +15,7 @@ type JsonRender struct{}
 
 func (j JsonRender) Render(writer http.ResponseWriter, v interface{}) error {
 	writeContentType(writer, jsonContentType)
-	data, err := json.Marshal(v)
+	data, err := js.Marshal(v)
 	if err != nil {
 		return err
 	}
@@ -38,3 +39,26 @@ var (
 	jsonRender = JsonRender{}
 	xmlRender  = XmlRender{}
 )
+
+type jsonTransformer interface {
+	Unmarshal(data []byte, v interface{}) error
+	Marshal(v interface{}) ([]byte, error)
+}
+
+type defaultJsonTransformer struct{}
+
+func (d defaultJsonTransformer) Unmarshal(data []byte, v interface{}) error {
+	return json.Unmarshal(data, v)
+}
+
+func (d defaultJsonTransformer) Marshal(v interface{}) ([]byte, error) { return json.Marshal(v) }
+
+var js jsonTransformer = defaultJsonTransformer{}
+
+func SetJsonTransformer(j jsonTransformer) error {
+	if j == nil {
+		return errors.New("jsonTransformer can not be nil")
+	}
+	js = j
+	return nil
+}
