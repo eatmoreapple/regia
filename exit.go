691b713692bea121ef9abd10c9e9e567a2fde35c
nieqing <919624032@qq.com>
2021-09-06 09:36:46 +0800

    add code note

--- exit.go
+++ exit.go
@@ -1,3 +1,7 @@
+// Copyright 2021 eatMoreApple.  All rights reserved.
+// Use of this source code is governed by a GPL style
+// license that can be found in the LICENSE file.
+
 package regia
 
 type Exit interface{ Exit(context *Context) }
@@ -6,5 +10,3 @@ type exit struct{}
 
 // Exit Do nothing
 func (e exit) Exit(*Context) {}
-
-
--- regia.go
+++ regia.go
@@ -1,5 +1,5 @@
 // Copyright 2021 eatMoreApple.  All rights reserved.
-// Use of this source code is governed by a GNU style
+// Use of this source code is governed by a GPL style
 // license that can be found in the LICENSE file.
 
 package regia
